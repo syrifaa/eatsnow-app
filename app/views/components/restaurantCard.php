061a6fd24195bcc06062b71ef1eb8ae3a55006ad
kennypanjaitan_ <13521023@std.stei.itb.ac.id>
2023-09-28 02:44:04 +0700

    feat: add restaurant Card and its component
    TODO:
    - Getting restaurant info from DB
    - Generating restaurant card
    - Restaurant pagination

---
+++ app/views/components/restaurantCard.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Restaurant Component</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='/public/css/restaurantCard.css'>
+</head>
+<body>
+    <!-- TODO: Change template text and image to getting restaurant info from DB -->
+    <div class="restaurant">
+        <img src="/public/assets/img/rest1.svg" alt="restoran" class="restaurant-img">
+        <div class="restaurant-info">
+            <div class="restaurant-name">Restaurant Name</div>
+            <div class="restaurant-category">Restaurant Category</div>
+            <div>
+                <img src="/public/assets/vectors/pinpoint.svg" alt="pinpoint">
+                <div>Jalan Raya, Cileunyi Wetan, Cileunyi, Bandung Regency, West Java 40622</div>
+            </div>
+            <div>
+                <img src="/public/assets/vectors/clock.svg" alt="clock">
+                <div>Restaurant Schedule</div>
+            </div>
+            <div >
+                <img src="/public/assets/vectors/review.svg" alt="review">
+                <div>Restaurant Review</div>
+            </div>
+            <div >
+                <img src="/public/assets/vectors/star.svg" alt="rating">
+                <div>Restaurant Rating</div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
