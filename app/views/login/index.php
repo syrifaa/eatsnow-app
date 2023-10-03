abc9d48581c7af4e698ffc136a27c7235ba8a72d
Agsha Athalla Nurkareem <agshaathalla@gmail.com>
2023-10-03 20:14:26 +0700

    feat: add signup and login page

---
+++ app/views/login/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <title>Login - EatsNow</title>
+        <link rel="stylesheet" href="../../../public/css/signupLogin.css">
+        <link rel="icon" type="image/png" href="../../../public/assets/img/logo.png"/>
+
+</head>
+<body>
+    <div class="signup">
+        <h1>Login</h1>
+        <form action="/api/login.php" method="POST">
+            <table>
+            <label>Email</label>
+            <input type="email" name="email"> <br>
+            <label>Password</label>
+            <input type="password" name="password"> <br>
+            <input type="submit" name="" value="Login">
+            </table><br>
+            <u>
+                Don't Have Account? click <a href="../signup/index.php" class="here">here</a> for sign up
+            </u>
+        </form>
+    </div>
+</body>
+</html>
---
+++ app/views/signup/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <title>Sign Up  - EatsNow</title>
+        <link rel="stylesheet" href="../../../public/css/signupLogin.css">
+        <link rel="icon" type="image/png" href="../../../public/assets/img/logo.png"/>
+
+</head>
+<body>
+    <div class="signup">
+        <h1>Sign Up</h1>
+        <form action="/api/signup.php" method="POST">
+            <table>
+            <label>Username</label>
+            <input type="text" name="name"> <br>
+            <label>Email</label>
+            <input type="email" name="email"> <br>
+            <label>Password</label>
+            <input type="password" name="password"> <br>
+            <input type="submit" name="" value="Sign Up" href="">
+            </table><br>
+            <u>
+                Already Have Account? click <a href="../login/index.php" class="here">here</a> for login
+        </u>
+        </form>
+    </div>
+</body>
+</html>
