8a05ef00ac2f5b7193bc8f1d3bfabffdb92d6965
syrifaa <13521018@std.stei.itb.ac.id>
2023-10-03 21:12:58 +0700

    feat: add profile page

---
+++ app/views/profile/index.php
@@ -0,0 +1,59 @@
+<?php
+$title  = "EatsNow";
+$page = "Profile";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+    <link rel="icon" type="image/png" href="../../../public/assets/img/logo.png"/>
+    <link rel="stylesheet" href="../../../public/css/profile.css"/>
+    <link rel="stylesheet" href="../../../public/css/navbar.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+</head>
+<body>
+    <section class="header">
+        <a href="#" class="logo">
+            <img src="../../../public/assets/img/logo1.png"/>
+        </a>
+        <nav class="navbar">
+            <?php include "../navbar/index.php"; ?>
+            <a href="login.php" class="login">Login</a>
+            <a href="signup.php" class="signup">SignUp</a>
+        </nav>
+        <div id ="menu-btn" class="fas fa-bars"></div>
+    </section>
+    <section class="container">
+        <div class="profile">
+            <div class="image">
+                <div class="profile-container">
+                    <img id="profileImage" src="../../../public/assets/img/profile-img.png"/>
+                </div>
+                <input class="imageUpload" type="file" 
+                    name="profile_photo" placeholder="Photo" required="" capture>
+            </div>
+            <div class="form">
+                <label for="name">Name</label><br>
+                <input type="text" class="input-form" name="name" required><br>
+                <label for="email">Email</label><br>
+                <input type="email" class="input-form" name="email" required><br>
+                <label for="pw">Change Password</label><br>
+                <input type="text" class="input-form" name="pw" required><br>
+            </div>
+        </div>
+        <div class="update-btn">
+            <a href="#" class="update">Update</a>
+        </div>
+    </section>
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        setupImageUpload('#profileImage', '.imageUpload');
+    });
+</script>
+<script src="../../../public/js/navbar.js"></script>
+<script src="../../../public/js/preview-img.js"></script>
+</body>
+</html>
