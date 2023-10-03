544620e1b3f13bccd39aca74ef70d855774b6d8d
Agsha Athalla Nurkareem <agshaathalla@gmail.com>
2023-10-03 22:32:00 +0700

    feat: database setting

---
+++ app/core/db.php
@@ -0,0 +1,34 @@
+<?php
+    class database{
+      private $conn;
+      private $statement;
+      
+        public function __construct(){
+            $this->conn = mysqli_connect ("db", "user", "password", "eatsnow");
+            if ($this->conn->connect_error) {
+                die("Connection failed: " . $this->conn->connect_error);
+            }
+        }
+
+        public function execute($query){
+            return mysqli_query($this->conn, $query);
+        }
+
+        public function selectAll($table){
+            $query = "SELECT * FROM $table";
+            $result = $this->execute($query);
+            $data = [];
+            while($row = mysqli_fetch_assoc($result)){
+                $data[] = $row;
+            }
+            return $data;
+        }
+
+        public function selectOne($table, $id){
+            $query = "SELECT * FROM $table WHERE id = $id";
+            $result = $this->execute($query);
+            return mysqli_fetch_assoc($result);
+        }
+    
+    }
+?>
